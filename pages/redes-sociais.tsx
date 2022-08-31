9d13c4e404ae9d37659971dd6f39acc93cf534f6
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-08-30 23:27:29 -0300

    redes sociais

---
+++ pages/redes-sociais.tsx
@@ -0,0 +1,35 @@
+const RedesSociais = () => {
+  return (
+    <ul>
+      <li>
+        <a
+          href="https://chat.whatsapp.com/HTt7FwitQ9v6GnJ0VywgQw"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Whatsapp
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://discord.gg/PjpYsPhtpx"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Discord
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.linkedin.com/groups/12670203/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+export default RedesSociais;
