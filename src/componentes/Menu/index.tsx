ac398c04fd0b6792f231effa9942547c5d3a781b
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-08-30 23:11:27 -0300

    estrura inicial menu

--- src/componentes/Menu/index.tsx
+++ src/componentes/Menu/index.tsx
@@ -1,5 +1,32 @@
+import Link from "next/link";
+
 const Menu = () => {
-  return <h1>Menu</h1>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/sobre">
+            <a>Sobre</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/redes-sociais">
+            <a>Redes Sociais</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/desafios">
+            <a>Desafios</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Menu;
