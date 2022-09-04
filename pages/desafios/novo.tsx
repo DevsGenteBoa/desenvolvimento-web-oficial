e7351c2d0505710548cd01b8c1cdd3796c83ce97
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-09-04 01:13:29 -0300

    melhorando descrição do incluir desafio

--- pages/desafios/novo.tsx
+++ pages/desafios/novo.tsx
@@ -5,7 +5,20 @@ const Novo = () => {
       <ol>
         <li>Resolva na sua máquina local</li>
         <li>Faça upload no seu github e deixe público</li>
-        <li>Faça um Pull Reques alterando o arquivo src/desafios-resolvidos.json com suas informações (basta seguir o padrão de objeto de JSON que tem lá)</li>
+        <li>
+          Acesse o{" "}
+          <a
+            href="https://github.com/PedroMarianoAlmeida/desenvolvimento-web-oficial"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            repositório oficial
+          </a>{" "}
+          e faça um Pull Request alterando o arquivo{" "}
+          <b>src/desafios-resolvidos.json</b> com suas informações (basta seguir
+          o padrão de objeto de JSON que tem lá)
+        </li>
+        <li>Informe para algum moderador</li>
       </ol>
     </>
   );
