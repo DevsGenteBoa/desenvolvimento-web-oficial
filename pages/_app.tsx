2e20350f7705e285aab4a7175c914e818416fcd2
Felipe Santos <rfelipe273@gmail.com>
2022-09-04 15:20:26 -0300

    add font inter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,21 @@
 import "styles/globals.css";
 import type { AppProps } from "next/app";
 import Menu from "src/componentes/Menu";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+
+        <title>Desenvolvimento Web</title>
+      </Head>
       <Menu />
       <Component {...pageProps} />
     </>
