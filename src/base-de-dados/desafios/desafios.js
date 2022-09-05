c9061260c8ef9a181a996955b5347f9355e0fe05
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-09-04 21:49:36 -0300

    inlcuindo segundo desafio exemplo

--- src/base-de-dados/desafios/desafios.js
+++ src/base-de-dados/desafios/desafios.js
@@ -5,9 +5,19 @@ const desafios = [
     resumo:
       "Crie um card de funcionários a partir de uma dada coleção de dados.",
     descricao: `Card de Funcionários Tendo um arquivo com os dados dos profissionais e uma lista de serviços com os dados a serem tratados, mostrar cards com as seguintes informações:
-    
+
     - Uma lista com o nome e valor de apenas serviços especiais que ele faz
     - Uma lista com nome e descrição de todos os serviços que ele faz
+    `,
+    link: "",
+    image: "",
+    tags: ["Manipulação Array", "Filtro", "HTML Select"],
+  },
+  {
+    id: 2,
+    titulo: "Desafio Teste",
+    resumo: "Teste",
+    descricao: `## Teste
     `,
     link: "",
     image: "",
