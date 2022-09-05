a813c3a49c90aee627747391868708329b00c73a
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-09-04 21:35:02 -0300

    leitor de markdown funcionando

--- pages/desafios/index.tsx
+++ pages/desafios/index.tsx
@@ -1,9 +1,11 @@
+import ReactMarkdown from "react-markdown";
+
 import { DesafioCard } from "src/components/cards/desafio-card";
 import { DesafiosGrid } from "src/components/Desafios/styles";
 import { Flex } from "src/components/flex";
 import Wrapper from "src/components/wrapper";
 
-import desafios from "src/base-de-dados/desafios/desafios.js";
+import desafios from "src/base-de-dados/desafios/desafios";
 
 const desafio_exemplo = {
   id: 1,
@@ -36,24 +38,28 @@ const desafio_exemplo = {
 const Desafios = () => {
   console.log(desafios);
   return (
-    <Flex>
-      <Wrapper
-        css="
+    <>
+      <ReactMarkdown>{desafios[0].descricao}</ReactMarkdown>
+
+      <Flex>
+        <Wrapper
+          css="
         display: flex;
         flex-direction: column;
         justify-content: center;
         gap: 20px;
         margin-top: 40px;
       "
-      >
-        <h1>Desafios</h1>
-        <DesafiosGrid>
-          {[1, 2, 3, 4, 5].map((i) => (
-            <DesafioCard key={i} desafio={desafio_exemplo} />
-          ))}
-        </DesafiosGrid>
-      </Wrapper>
-    </Flex>
+        >
+          <h1>Desafios</h1>
+          <DesafiosGrid>
+            {[1, 2, 3, 4, 5].map((i) => (
+              <DesafioCard key={i} desafio={desafio_exemplo} />
+            ))}
+          </DesafiosGrid>
+        </Wrapper>
+      </Flex>
+    </>
   );
 };
 
--- src/components/DesafiosResolvidos.tsx
+++ src/components/DesafiosResolvidos.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+
+
 import desafios from "src/base-de-dados/desafios/desafios-resolvidos.json";
 
 interface DesafiosResolvidosProps {
